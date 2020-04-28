4a0371689eb2b5b0dda366cbea70a7f51063cb9d
Svilen Stavrev <svilen@etherino.net>
2020-04-28 11:51:59 +0300

    Some checks added

--- finduple.py
+++ finduple.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import re
 
-# there are no questions with more possible answers than this
+# there are no questions with more or less possible answers than this
 MAX_ANSWERS = 6
+MIN_ANSWERS = 3
 re_illegal_chars = re.compile('[\n\r\t]')
 
+
 class Question:
-    def __init__(self, nmb_arg=-1, qid_arg=-1, qst_arg='', ans_arg={}):
+    def __init__(self, nmb_arg=-1, qid_arg=-1, qst_arg='', ans_args={}, bt_arg=''):
         self.__nmb = nmb_arg
         self.__qid = qid_arg
         self.__question = qst_arg
-        self.__answers = ans_arg
+        self.__answers = ans_args
+        self.__brieftext = bt_arg
 
     def get_nmb(self):
         return self.__nmb
@@ -28,6 +31,10 @@ class Question:
         return self.__answers
     answers = property(get_answers)
 
+    def get_brieftext(self):
+        return self.__brieftext
+    brieftext = property(get_brieftext)
+
     # Check the answers for illegal characters, empty text, duplicate text
     def check_answers_ic(self):
         result = 'OK'
@@ -37,6 +44,17 @@ class Question:
                 result = 'FAIL: Illegal charracters found in answers'
                 break
         return result
+
+
+def print_question(qst):
+    print('NMB={0}\tQID={1}\t{2}'.format(qst.nmb, qst.qid, qst.brieftext))
+    print('Question: ' + qst.question)
+    n = 1
+    for ans in qst.answers:
+        print('   {0}: {1} \t\tSCR:{2}'.format(n, ans, qst.answers[ans]))
+        n+=1
+    print()
+    return
 
 
 # Read Excel file and put data in to a DataFrame
@@ -57,6 +75,7 @@ for qst in df1['QID']:
         # print('QID=', df1['QID'][row])
         qid = int(df1['QID'][row])
         # print('QUESTION: ', df1['QUESTION/ANSWER'][row])
+        bt = df1['BRIEFTEXT'][row]
         qst_text = df1['QUESTION/ANSWER'][row]
         qst_text = qst_text.strip(" \n\r?:.")
         # retrieve the answers
@@ -75,20 +94,33 @@ for qst in df1['QID']:
                 print('WARNING! Duplicate answer in question NMB={0} QID={1}'.format(nmb, qid))
             answers[answer_text]=int(answer_score)
             a += 1
-        q = Question(nmb, qid, qst_text, answers)
+        q = Question(nmb, qid, qst_text, answers, bt)
         questions.append(q)
     row += 1
 print('Parsed {0} questions in the file.\n\n'.format(len(questions)))
 
-# CHECK: for illegal characters
+# ----------- CHECKS ------------
 for qst in questions:
+    fail = False
+    # CHECK: for illegal characters
     if qst.check_answers_ic() != 'OK':
-        print(qst.nmb, ' ', qst.qid, ' ', qst.question)
         print(qst.check_answers_ic())
+        fail = True
+    # CHECK: for too many answers
+    if len(qst.answers) == MAX_ANSWERS:
+        print('WARNING: Answers number is at MAX!')
+        fail = True
+    # Check: for min answers
+    if len(qst.answers) < MIN_ANSWERS:
+        print('WARNING: Answers number is below MIN!')
+        fail = True
+    if fail:
+        print_question(qst)
         print()
 
 # CHECK: for duplicates
 # dictionary of all duplicated questions NMB:QID
+
 duplicates = {}
 dup_counter = 0
 for qst in questions:
@@ -103,7 +135,9 @@ for qst in questions:
             continue
         q1_txt = qst.question.lower()
         q2_txt = qst2.question.lower()
-        if q1_txt == q2_txt:
+        q1_bt = qst.brieftext
+        q2_bt = qst.brieftext
+        if (q1_bt + q1_txt) == (q2_bt + q2_txt):
             dup_counter += 1
             duplicates[qst.nmb] = qst.qid
             duplicates[qst2.nmb] = qst2.qid
@@ -111,3 +145,4 @@ for qst in questions:
             dup[qst2.nmb] = qst2.qid
     if len(dup) != 0:
         print('DUP {0}: {1}'.format(dup_counter-1, dup))
+
